ba837db2228c5f9017836572bc1eed8285b3b069
hthuz <hanggang.20@intl.zju.edu.cn>
2023-07-02 21:07:32 +0800

    Add: fcitx5 config

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ ln -fsnv "${DIR}/.gitconfig" ~
 ln -fsnv "${DIR}/.bashrc" ~
 ln -fsnv "${DIR}/.bash_profile" ~
 
+
+FCITX5_PATH="config/fcitx5/conf"
+ln -fsnv "${DIR}/${FCITX5_PATH}/pinyin.conf" ~/.${FCITX5_PATH}
+ln -fsnv "${DIR}/${FCITX5_PATH}/classicui.conf" ~/.${FCITX5_PATH}
+FCITX5_LOCAL="local/share/fcitx5/themes"
+cp -r "${DIR}/${FCITX5_LOCAL}/Perl-White" ~/.${FCITX5_LOCAL}
+

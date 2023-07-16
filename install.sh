e50dedffa37cd0c1cf0dac5b1b649626a7619336
hthuz <hanggang.20@intl.zju.edu.cn>
2023-07-16 18:38:13 +0800

    Add: judge if path exists

--- install.sh
+++ install.sh
@@ -8,9 +8,12 @@ ln -fsnv "${DIR}/.bash_profile" ~
 ln -fsnv "${DIR}/.keys.conf" ~
 
 
-FCITX5_PATH="config/fcitx5/conf"
-ln -fsnv "${DIR}/${FCITX5_PATH}/pinyin.conf" ~/.${FCITX5_PATH}
-ln -fsnv "${DIR}/${FCITX5_PATH}/classicui.conf" ~/.${FCITX5_PATH}
-FCITX5_LOCAL="local/share/fcitx5/themes"
-cp -r "${DIR}/${FCITX5_LOCAL}/Perl-White" ~/.${FCITX5_LOCAL}
+FCITX5_PATH=".config/fcitx5/conf"
+if [[ -e ~/${FCITX5_PATH} ]]
+then
+    ln -fsnv "${DIR}/${FCITX5_PATH}/pinyin.conf" ~/${FCITX5_PATH}
+    ln -fsnv "${DIR}/${FCITX5_PATH}/classicui.conf" ~/${FCITX5_PATH}
+    FCITX5_LOCAL=".local/share/fcitx5/themes"
+    cp -r "${DIR}/${FCITX5_LOCAL}/Perl-White" ~/${FCITX5_LOCAL}
+fi
 
